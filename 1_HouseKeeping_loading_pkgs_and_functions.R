620dadcf14def2f02f887fd651004cd49ea558ae
Meadhbh Moriarty <mmoriar@tcd.ie>
2016-06-17 09:40:44 +0100

    Update 1_HouseKeeping_loading_pkgs_and_functions.R
    New notes added and new function added here.

--- 1_HouseKeeping_loading_pkgs_and_functions.R
+++ 1_HouseKeeping_loading_pkgs_and_functions.R
@@ -187,6 +187,23 @@ replace_function=function(DT){
     set(DT, i = which(DT[[cname]] == -9), j = cname, value = NA)
   }
 }
+# Random number generator
+rand_vect <- function(N, M, sd = 1, pos.only = TRUE) {
+  vec <- rnorm(N, M/N, sd)
+  if (abs(sum(vec)) < 0.01) vec <- vec + 1
+  vec <- round(vec / sum(vec) * M)
+  deviation <- M - sum(vec)
+  for (. in seq_len(abs(deviation))) {
+    vec[i] <- vec[i <- sample(N, 1)] + sign(deviation)
+  }
+  if (pos.only) while (any(vec < 0)) {
+    negs <- vec < 0
+    pos  <- vec > 0
+    vec[negs][i] <- vec[negs][i <- sample(sum(negs), 1)] + 1
+    vec[pos][i]  <- vec[pos ][i <- sample(sum(pos ), 1)] - 1
+  }
+  vec
+}
 #########################
 # Set Working Directory #
 #########################
